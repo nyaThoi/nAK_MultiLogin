6df5b0d3bb99a62a2c60c921dd550c0f35c74fd3
nTiean <notdefine@error.404>
2022-01-29 00:54:54 +0100

    ES URL

--- nAK_MC/URL.cs
+++ nAK_MC/URL.cs
@@ -17,6 +17,6 @@ namespace nAK_MC
         public static readonly string DE_URL = "https://www.aeriagames.com/dialog/oauth?response_type=code&client_id=cd9391da898949ca3be24b54ecad7bad0531f0feb&state=xyz&lang=de";
         public static readonly string US_URL = "https://www.aeriagames.com/dialog/oauth?response_type=code&client_id=18457ae92576b65ab92213612f6cc02d051ef19d2&state=xyz&lang=us";
         public static readonly string FR_URL = "https://www.aeriagames.com/dialog/oauth?response_type=code&client_id=54f8336864c92199433c23b143cf0fa4051ef1a81&state=xyz&lang=fr";
-        public static readonly string ES_URL = "Missing URL...";
+        public static readonly string ES_URL = "https://www.aeriagames.com/dialog/oauth?response_type=code&client_id=b79ffdae0ebc7624d4c7b34234688171054082acb&state=xyz&lang=es";
     }
 }        
